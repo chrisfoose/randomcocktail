ef78f1b2ca261aaae36bceca40d284eb4f60310a
Christopher Foose <chrisfoose@gmail.com>
2022-04-18 16:14:14 -0700

    got the ingredients to work

--- script.js
+++ script.js
@@ -16,34 +16,38 @@ fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
       const cocktail = data.drinks[0];
       const cocktailDiv = document.getElementById('cocktail');
       const cocktailName = cocktail.strDrink;
-  const heading = document.createElement('h1');
-  heading.innerHTML = cocktailName;
-  cocktailDiv.appendChild(heading);
 
-  const cocktailImg = document.createElement("img");
-  cocktailImg.src = cocktail.strDrinkThumb;
-  cocktailDiv.appendChild(cocktailImg);
-  document.body.style.backgroundImage = "url('" + cocktail.strDrinkThumb + "')";
+      const heading = document.createElement('h1');
+      heading.innerHTML = cocktailName;
+      cocktailDiv.appendChild(heading);
 
-  const cocktailIngredients = document.createElement('ul');
-  cocktailDiv.appendChild(cocktailIngredients);
+      const cocktailImg = document.createElement("img");
+      cocktailImg.src = cocktail.strDrinkThumb;
+      cocktailDiv.appendChild(cocktailImg);
+      document.body.style.backgroundImage = "url('" + cocktail.strDrinkThumb + "')";
+ 
+      const cocktailIngredients = document.createElement('ul');
+      cocktailDiv.appendChild(cocktailIngredients);
 
-  const getIngredients = Object.keys(cocktail)
-    .filter(function (ingredient) {
-        return ingredient.indexOf("strIngredient") == 0;
-    })
-    .reduce(function (ingredient, ingredient) {
-        if (cocktail[ingredient] != null) {
-            ingredients[ingredient] = cocktail[ingredient];
+      const getIngredients = Object.keys(cocktail)
+        .filter(function (ingredient) {
+            return ingredient.indexOf("strIngredient") == 0;
+        })
+        .reduce(function (ingredients, ingredient) {
+            if (cocktail[ingredient] != null) {
+                ingredients[ingredient] = cocktail[ingredient];
+            }
+            return ingredients;
+        }, {});
+
+        for (let key in getIngredients) {
+            let value = getIngredients[key];
+            listItem = document.createElement("li");
+            listItem.innerHTML = value;
+            cocktailIngredients.appendChild(listItem);
         }
-        return ingredients;
-    }, {});
-
-    for (let key in getIngredients) {
-        let value = getIngredients[key];
-        listItem = document.createElement("li");
-        listItem.innerHTML = value;
-        cocktailIngredients.appendChild(listItem);
-    }
 
   }
+
+ 
+
