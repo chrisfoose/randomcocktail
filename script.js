b33ea2376a4a3ae7f1b5278fb365a4ce5ee54931
Christopher Foose <chrisfoose@gmail.com>
2022-04-18 15:59:48 -0700

    put stuff in the function

--- script.js
+++ script.js
@@ -15,9 +15,7 @@ fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
   function displayCocktail(data) {
       const cocktail = data.drinks[0];
       const cocktailDiv = document.getElementById('cocktail');
-  }
-
-  const cocktailName = cocktail.strDrink;
+      const cocktailName = cocktail.strDrink;
   const heading = document.createElement('h1');
   heading.innerHTML = cocktailName;
   cocktailDiv.appendChild(heading);
@@ -47,3 +45,5 @@ fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
         listItem.innerHTML = value;
         cocktailIngredients.appendChild(listItem);
     }
+
+  }
